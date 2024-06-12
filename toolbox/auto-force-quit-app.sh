3ca3404e46091da402239b857acfdfa7914e2a47
Daniel <116264071+DDC-96@users.noreply.github.com>
2024-06-12 12:36:57 -0700

    Update auto-force-quit-app.sh

--- toolbox/auto-force-quit-app.sh
+++ toolbox/auto-force-quit-app.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 
-# Force quits the app before uninstalling
+# Force quits the app if at a failed or frozen state 
 # In Jamf, add Application path to $4 column 
 
 ps aux | grep "$4" | grep -v "grep" | awk '{print $2}' | xargs kill -15
