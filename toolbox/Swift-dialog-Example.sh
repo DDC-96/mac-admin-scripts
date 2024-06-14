1eedb584920aa915a99080b01234c051d671d123
Daniel <116264071+DDC-96@users.noreply.github.com>
2024-06-13 18:02:54 -0700

    Update Swift-dialog-Example.sh

--- toolbox/Swift-dialog-Example.sh
+++ toolbox/Swift-dialog-Example.sh
@@ -1,3 +1,3 @@
 # swift dialog 1 liner 
-# Needs swift dialog installed on the machine first to test
+# Needs swift dialog installed on the machine first to run
 dialog --message "Example Description Here." --title "Example Title Here" --style mini centered --icon "/Applications/Self Service.app" --buttonstyle centre
